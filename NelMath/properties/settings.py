2900b9465c6f798be5432d3c1e934dca20290999
Михаил Бакиновский <wolfsector207@gmail.com>
2024-11-24 23:28:58 +0200

    settings remastering[wip]

--- NelMath/properties/settings.py
+++ NelMath/properties/settings.py
@@ -1,11 +1,11 @@
 settings={
-    
 #math base settings
-'mm_dynamic_class_changing': True,
+    'mm_max_float_part':4,
+    'mm_dynamic_class_changing': False,
 #crypto settings
-'mm_minimum_key_value' : 2,
-'mm_maximum_key_value' : 2147483648,
-'mm_key_only_primals' : True,
-'mm_key_enable_number_repeating' : True,
-'mm_key_maximum_repeat_count' : -1,
+    'mm_minimum_key_value' : 2,
+    'mm_maximum_key_value' : 2147483648,
+    'mm_key_only_primals' : True,
+    'mm_key_enable_number_repeating' : True,
+    'mm_key_maximum_repeat_count' : -1,
 }
--- NelMath/properties/settings_handler.py
+++ NelMath/properties/settings_handler.py
@@ -3,9 +3,6 @@ class SettingsHandler():
     __created = False
     __custom_settings_file = ''
     __settings={}
-    if __settings=={}:
-        from .settings import settings
-        __settings=settings
 
 
     def __new__(cls, custom_settings_file_path:str=''):
@@ -16,16 +13,30 @@ class SettingsHandler():
     def __init__(self, custom_settings_file_path:str=''):
         if not self.__created:
             self.__custom_settings_file = custom_settings_file_path
-            self.__created = True
+            self.__created = True            
+            if SettingsHandler.__settings=={}:
+                from .settings import settings
+                SettingsHandler.__settings=settings
 
     def __check_settings(self, path=''):
         pass
+
+    @staticmethod
+    def save_working_settings():
+        SettingsHandler.__working=SettingsHandler.__settings
+
+    @staticmethod
+    def release_working_settings():
+        if SettingsHandler.__working:
+            SettingsHandler.__settings=SettingsHandler.__working
+            del(SettingsHandler.__working)
     
     @staticmethod
     def release_settings_file(path):
         '''
         generates settings-file on custom path to change settings manually
         '''
+        pass
         
     def update_settings_from_file(self, path):
         '''
@@ -35,6 +46,7 @@ class SettingsHandler():
         ---If some setting has an error handler will use default values
         --All precision settings that present in programm both file will update if they correct
         '''
+        pass
     
     @staticmethod
     def get(key):
@@ -44,3 +56,25 @@ class SettingsHandler():
         if key in SettingsHandler.__settings.keys():
             return SettingsHandler.__settings[key]
         return None
+
+    def set_defaults(self)->None:
+        '''
+        set settings to a default values for a working space
+        '''
+        default_settings={
+                'mm_max_float_part':4,
+                'mm_dynamic_class_changing': False,
+                'mm_minimum_key_value' : 2,
+                'mm_maximum_key_value' : 2147483648,
+                'mm_key_only_primals' : True,
+                'mm_key_enable_number_repeating' : True,
+                'mm_key_maximum_repeat_count' : -1,
+            }
+        self.__settings=default_settings
+
+    def change_setting(self, key:str, value:any)->None:
+        '''
+        changes a setting not applied this to a setting file. Useful for testing or for some situations
+        '''
+        if key in self.__settings.keys():
+            self.__settings[key]=value
